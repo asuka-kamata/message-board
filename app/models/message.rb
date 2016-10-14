f4ecd722e54ce5926540b4eb06e38858ec0e18ea
asuka kamata <etoileazukiwestgodjp1122.2211@gmail.com>
2016-10-14 16:18:36 +0000

    20161015-03 add age

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,8 @@
 class Message < ActiveRecord::Base
     #名前は必須入力かつ２０文字以内
     validates :name, length: { maximum: 20 }, presence: true
+    #数値であり、０以上の場合有効
+    validates :age, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
     #内容は必須入力かつ２文字以上３０文字以下
     validates :body, length: { minimum: 2, maximum: 30 }, presence: true
 end
